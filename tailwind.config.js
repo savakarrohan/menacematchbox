6c3af59dbc8370ab2ae8ff30e04fdd04a1137716
Savakar <savakarrohan@gmail.com>
2024-03-20 11:34:00 +0530

    feat: tailwind working

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+// tailwind.config.js
+module.exports = {
+  purge: ["./src/**/*.tsx"], // Specify your React component files
+  theme: {},
+  variants: {},
+  plugins: [],
+};
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+// webpack.config.js
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx", // Your React entry point
+  mode: "development",
+  output: {
+    filename: "index_bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+      chunkFilename: "styles.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  devtool: "eval-source-map",
+};
