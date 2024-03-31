833134a8f913abb96338a49450bb845ad6b72696
Savakar <savakarrohan@gmail.com>
2024-04-01 00:12:49 +0530

    feat : implement the boards

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,13 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Table, Checkbox } from "antd";
+import { board } from "../interfaces";
+import { xTurn } from "../helpers";
 
 export function TicTacToe() {
-  const data = [
-    { key: "row1", col1: <Checkbox />, col2: <Checkbox />, col3: <Checkbox /> },
-    { key: "row2", col1: <Checkbox />, col2: <Checkbox />, col3: <Checkbox /> },
-    { key: "row3", col1: <Checkbox />, col2: <Checkbox />, col3: <Checkbox /> },
-  ];
+  const [board, setBoard] = useState<board>(xTurn);
   return (
     <>
       <div className="flex justify-center">
@@ -15,35 +13,35 @@ export function TicTacToe() {
           <tbody>
             <tr>
               <td id="pos0" className="p0">
-                <Checkbox id="1,1" />
+                {board[0][0] != "" ? board[0][0] : <Checkbox id="0,0" />}
               </td>
               <td id="pos1" className="p1">
-                <Checkbox id="1,2" />
+                {board[0][1] != "" ? board[0][1] : <Checkbox id="0,1" />}
               </td>
               <td id="pos2" className="p2">
-                <Checkbox id="1,3" />
+                {board[0][2] != "" ? board[0][2] : <Checkbox id="0,2" />}
               </td>
             </tr>
             <tr>
               <td id="pos3" className="p3">
-                <Checkbox id="2,1" />
+                {board[1][0] != "" ? board[1][0] : <Checkbox id="1,0" />}
               </td>
               <td id="pos4" className="p4">
-                <Checkbox id="2,1" />
+                {board[1][1] != "" ? board[1][1] : <Checkbox id="1,1" />}
               </td>
               <td id="pos5" className="p5">
-                <Checkbox id="2,1" />
+                {board[1][2] != "" ? board[1][2] : <Checkbox id="1,2" />}
               </td>
             </tr>
             <tr>
-              <td id="pos6" className="p6">
-                <Checkbox id="3,1" />
+              <td id="pos6" className="p3">
+                {board[2][0] != "" ? board[2][0] : <Checkbox id="2,0" />}
               </td>
-              <td id="pos7" className="p7">
-                <Checkbox id="3,1" />
+              <td id="pos7" className="p4">
+                {board[2][1] != "" ? board[2][1] : <Checkbox id="2,1" />}
               </td>
-              <td id="pos8" className="p8">
-                <Checkbox id="3,1" />
+              <td id="pos8" className="p5">
+                {board[2][2] != "" ? board[2][2] : <Checkbox id="2,2" />}
               </td>
             </tr>
           </tbody>
